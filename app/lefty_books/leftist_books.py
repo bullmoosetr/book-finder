95e0839852a6bcc825ed9ee46c954b13bafd8c99
bullmoosetr <ericlaff16@gmail.com>
2020-11-02 17:43:54 +0100

    Added another tab of book options

--- app/lefty_books/leftist_books.py
+++ app/lefty_books/leftist_books.py
@@ -6,7 +6,9 @@ leftist_books = Blueprint('leftist_books', __name__)
 
 @leftist_books.route('/')
 def index():
-    books = BaseRequest(search='communism', subject='communism', author='marx').search_by_volumes()
+    book_list = {}
+    marxist_books = BaseRequest(search='marx', subject='philosophy', author='marx').search_by_volumes()
+    communism = BaseRequest(search='communism', subject='communism').search_by_volumes_no_author()
     
     # What to display on index
     # Need Book Title
@@ -16,6 +18,7 @@ def index():
     # TODO Search by Zip code what stores sell the books - Will likely require either a web spider or api access either way need research
     
     # The list contains a list of dictionaries with Title, description, Author
-    book_list = [i.get('volumeInfo') for i in books.get('items')]
+    book_list['marxism'] = [i.get('volumeInfo') for i in marxist_books.get('items')]
+    book_list['communism'] = [i.get('volumeInfo') for i in communism.get('items')]
     return render_template('lefty_books/index.html', books=book_list)
 
